10b178333e604ab31bb9d54e8b34c39dc6487890
danyal <abbasidani82@gmail.com>
2024-06-03 16:14:34 +0500

    done

--- app/components/MainWeather.tsx
+++ app/components/MainWeather.tsx
@@ -1,12 +1,15 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { images } from 'assets'
-import { getHeight, getWidth } from 'lib'
+import { getFontSize, getHeight, getWidth } from 'lib'
+import { AppliedTheme } from 'themes'
 
+const theme=AppliedTheme();
 const MainWeather = () => {
   return (
     <View>
       <Image source={images.cloudRain}  style={styles.mainImage}/>
+      <Text style={styles.heading}>20</Text>
     </View>
   )
 }
@@ -15,10 +18,16 @@ export default MainWeather
 
 const styles = StyleSheet.create({
   mainImage:{
-    width:getWidth(40),
-    height:getHeight(30),
     resizeMode:'cover',
+    width:getWidth(40),
+    height:getWidth(40),
 
+  },
+  heading:{
+    color:theme.text.primary,
+    fontSize:getFontSize(16),
+    fontWeight:'600',
+    textAlign:'center'
   }
 
 })
